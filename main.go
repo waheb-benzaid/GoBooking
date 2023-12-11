837b1befcff1ea27920612e00460f28fb1489639
waheb-benzaid <waheb.benzaid@gmail.com>
2023-12-11 21:02:38 +0100

    add some logic

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
+	 conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	 remainingTickets := 50
+
+	fmt.Printf("Welcome to %v booking application\n",conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n",conferenceTickets,remainingTickets)
+	fmt.Println("Get your tocket here to attend!")
+	
+	var userName string
+	var userTickets int
+	//Ask the user for his name
+	
+	userName="Tom"
+	userTickets = 5
+
+	fmt.Printf("User %v booked %v tickets \n", userName, userTickets)
 }
